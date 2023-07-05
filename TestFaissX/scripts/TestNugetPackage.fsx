3a9bb74f4e175697293d01b036ff41b5e81cce0d
Faisal Waris <FaisalWaris@live.com>
2023-07-05 19:50:33 -0400

    working nuget packaging

---
+++ TestFaissX/scripts/TestNugetPackage.fsx
@@ -0,0 +1,10 @@
+﻿//test nuget package create here, locally
+//need nuget config for local packages
+#r "nuget: FaissNet"
+
+let m1 = new FaissNet.IndexFlat(20,FaissNet.MetricType.METRIC_L2)
+let v1 = [|1..20|] |> Array.map(fun i -> if i % 2 = 0 then 1.0f else -1.0f)
+m1.Add([|v1|])
+let count = m1.Count()
+m1.Dispose()
+
